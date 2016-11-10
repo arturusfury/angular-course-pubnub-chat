f65d47b4997059cb2b78232de24daa6a3e92499d
arturusfury <bri.reynolds@gmail.com>
2016-11-10 11:33:36 -0600

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCoursePubnubChatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularCoursePubnubChatApp
+ */
+angular.module('angularCoursePubnubChatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCoursePubnubChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCoursePubnubChatApp
+ */
+angular.module('angularCoursePubnubChatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
